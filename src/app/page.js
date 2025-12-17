ff7659631a302c689b1a64d6cfd261142fc08302
Rishabh Kumar <rishabrbl2005@gmail.com>
2025-12-17 11:46:08 +0530

    SignUp

---
+++ src/app/page.js
@@ -0,0 +1,129 @@
+import Image from "next/image";
+export default function HomePage() {
+  return (
+    <main className="flex bg-white">
+      <div className="h-screen basis-1/2 text-black flex flex-col items-center justify-center gap-4" >
+        <div className="flex items-center gap-2 w-2/3">
+          <Image
+            src="/images/briefcase1.svg"
+            alt="Logo"
+            width={30}
+            height={30}
+          />
+          <p className="font-bold">MyJob</p>
+        </div>
+        <div className="w-2/3 gap-4 flex flex-col text-[14px]">
+          <div className="flex justify-between items-center gap-4">
+            <div className="flex flex-col gap-2">
+              <div className="text-[24px] font-bold">Create account.</div>
+              <div className="text-[12px] text-gray-600">Already have an account? <span className="text-indigo-500">Log in</span></div>
+            </div>
+            <div className="border-1 border-solid rounded-lg p-2 border-gray-400 text-gray-700 flex">Employee <Image src="/images/fi_chevron-down.png" alt="Arrow Down" width={20} height={10} className="ml-2" /></div>
+          </div>
+          <div className="flex gap-2">
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="basis-1/2 border-1 border-solid border-gray-400 rounded-lg text-black-400 p-2"
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              className="basis-1/2 border-1 border-solid border-gray-400 rounded-lg text-black-400 p-2"
+            />
+          </div>
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="border-1 border-solid border-gray-400 rounded-lg text-black-400 p-2"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="border-1 border-solid border-gray-400 rounded-lg text-black-400 p-2"
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className="border-1 border-solid border-gray-400 rounded-lg text-black-400 p-2"
+          />
+          <div className="flex">
+            <input type="checkbox" className="mr-2" />
+            <span className="text-gray-500 text-sm">I've read and agree with your <span className="text-indigo-500 ">Terms of Service</span></span>
+          </div>
+          <div className="flex bg-indigo-600 text-white justify-center text-center p-3 rounded-sm cursor-pointer hover:bg-indigo-700">
+            Create Account <Image src="/images/fi_arrow-right.png" alt="Arrow Side" width={20} height={10} className="ml-2" />
+          </div>
+          <p className="self-center text-gray-500">or</p>
+          <div className="flex gap-2 text-gray-700">
+            <div className="flex basis-1/2 items-center justify-center gap-2 border-1 border-gray-300 border-solid rounded-lg p-2 cursor-pointer hover:bg-gray-100">
+              <Image
+                src="/images/Vector.svg"
+                alt="Facebook Sign Up"
+                width={10}
+                height={10}
+              />
+              <span> Sign up with Facebook</span>
+            </div>
+            <div className="flex basis-1/2 items-center justify-center gap-2 border-1 border-gray-300 border-solid rounded-lg p-2 cursor-pointer hover:bg-gray-100">
+              <Image
+                src="/images/Employers Logo.svg"
+                alt="Google Sign Up"
+                width={20}
+                height={20}
+              />
+              <span> Sign up with Google</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+      <div className="flex bg-[url('/images/bglogin.png')] bg-cover h-screen basis-1/2" style={{
+        clipPath: "polygon(2% 0, 100% 0, 100% 100%, 0 100%)"
+      }}>
+        <div className="w-full h-full bg-gradient-to-b from-[#041A3C80] to-[#041A3C] justify-end items-center flex flex-col text-white gap-6">
+          <div className="h-1/2 w-1/2 flex flex-col gap-6 justify-center">
+            <div className="text-[28px]">Over 1,75,324 Candidates waiting for good employee.</div>
+            <div className="flex justify-between">
+              <div className="flex flex-col justify-center">
+                <Image
+                  src="/images/briefcase-duotone 1 (1).svg"
+                  alt="Quote"
+                  width={50}
+                  height={50}
+                  className="bg-gray-700 rounded-lg p-2 mb-4"
+                  />
+                <span className="text-[14px]">1,75,324</span>
+                <span className="text-[12px] text-gray-300">Live Job</span>
+              </div>
+              <div className="flex flex-col justify-center">
+                <Image
+                  src="/images/buildings-duotone 1.svg"
+                  alt="Quote"
+                  width={50}
+                  height={50}
+                  className="bg-gray-700 rounded-lg p-2 mb-4"
+                  />
+                <span className="text-[14px]">97,354</span>
+                <span className="text-[12px] text-gray-300">Companies</span>
+              </div>
+              <div className="flex flex-col justify-center">
+                <Image
+                  src="/images/briefcase-duotone 1 (1).svg"
+                  alt="Quote"
+                  width={50}
+                  height={50}
+                  className="bg-gray-700 rounded-lg p-2 mb-4"
+                />
+                <span className="text-[14px]">7,532</span>
+                <span className="text-[12px] text-gray-300">New Jobs</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
