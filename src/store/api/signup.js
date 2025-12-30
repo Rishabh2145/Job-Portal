618fa4856e4ed2d2200cd1451f0c339493b6c5c1
Rishabh Kumar <rishabrbl2005@gmail.com>
2025-12-30 11:10:30 +0530

    Frontend

--- src/store/api/signup.js
+++ src/store/api/signup.js
@@ -2,6 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const userSignup = createAsyncThunk('signup/userSignup', async (data)=>{
+    console.log(data)
     const res = await axios.post(`${process.env.NEXT_PUBLIC_API}/signup`).then((res)=>{
         console.log(res.data)
         alert("Account created successfully! Please login with the same credentials")
--- src/store/slice/signup.js
+++ src/store/slice/signup.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import reducer from "./contact";
 import { userSignup } from "../api/signup";
 
 const initialState = {
