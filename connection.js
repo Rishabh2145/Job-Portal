fabcba4ae54131548865b0e784181f51ea3e562c
Rishabh Kumar <rishabrbl2005@gmail.com>
2025-12-30 12:14:07 +0530

    Mongo_modify

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ const { DATABASE_NAME } = require('./utils/constant');
 
 const connectDB = async () => {
   try {
-    const mongo = await mongoose.connect(`${process.env.MONGO_URL}/${DATABASE_NAME}`);
+    const mongo = await mongoose.connect(`${process.env.MONGO_URL}`);
     console.log("MongoDB connected on", mongo.connection.host);
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
